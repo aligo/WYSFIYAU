e63ec9410ea115c9e4f1f8fb4a4024a696d4a29a
aligo Kang <aligo_x@163.com>
2020-03-31 16:29:26 +0800

    t params to avoid browser cache

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+let params = {};
+let urlParts = (window.location.href + '?').split('?');
+urlParts[1].split('&').forEach((pair) => {
+  pair = (pair + '=').split('=').map(decodeURIComponent);
+  if (pair[0].length) {
+    params[pair[0]] = pair[1];
+  }
+});
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+if (!params.t || (params.t < (Date.now() - 600000))) {
+  params.t = Date.now();
+  let newUrl = urlParts[0] + '?';
+  for (const k in params) {
+    newUrl = newUrl + encodeURIComponent(k) + '=' + encodeURIComponent(params[k]) + '&';
+  }
+  window.location.href = newUrl.slice(0, -1);
+} else {
+  Vue.config.productionTip = false;
+  
+  new Vue({
+    render: h => h(App),
+  }).$mount('#app')
+}
